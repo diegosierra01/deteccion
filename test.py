2b4b4ac1768d9bc021b0d5f8c54f5dc8869a8745
larry852 <larryportocarrero@gmail.com>
2017-03-26 19:02:47 -0500

    Arregada linea 60

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ imagen = cv2.imread('rompecabezas3.jpg')
 # imagen = cv2.imread('colores.png')
 hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
 
-#Imagen resultado
+# Imagen resultado
 imagenres = Image.open("rompecabezas3.jpg")
-dibujo = ImageDraw.Draw(imagenres) 
+dibujo = ImageDraw.Draw(imagenres)
 
 # Rango de colores detectados:
 # Verdes:
@@ -57,7 +57,7 @@ blur = cv2.GaussianBlur(mask, (5, 5), 0)
 edges = cv2.Canny(mask, 1, 2)
 
 # Si el area blanca de la mascara es superior a 500px, no se trata de ruido
-contours, hier = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+_, contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 areas = [cv2.contourArea(c) for c in contours]
 i = 0
 for extension in areas:
@@ -71,56 +71,56 @@ for extension in areas:
     print(str(approx).replace('[[', '(').replace(']]', ')').replace('([', ' (').replace(')]', ')'))
     print()
 
-#fuente = ImageFont.truetype("Arabic Magic.ttf", 40)   
-#Metodo para encontrar el maximo y el minimo en x,y de todas las figuras
+#fuente = ImageFont.truetype("Arabic Magic.ttf", 40)
+# Metodo para encontrar el maximo y el minimo en x,y de todas las figuras
 for contour in contours:
     xmax = 0
     ymax = 0
     xmin, ymin, channels = imagen.shape
     for points in contour:
-        if xmax < points[:,0]:
-            xmax = points[:,0] 
-        if xmin > points[:,0]:
-            xmin = points[:,0] 
-        if ymax < points[:,1]:
-            ymax = points[:,1] 
-        if ymin > points[:,1]:
-            ymin = points[:,1]
-    #print('xmax '+str(xmax)+'ymax '+str(ymax)+'xmin '+str(xmin)+'ymin '+str(ymin)) 
-    if xmax-xmin > ymax-ymin:
-        differencemax = (xmax-xmin/2)+10
+        if xmax < points[:, 0]:
+            xmax = points[:, 0]
+        if xmin > points[:, 0]:
+            xmin = points[:, 0]
+        if ymax < points[:, 1]:
+            ymax = points[:, 1]
+        if ymin > points[:, 1]:
+            ymin = points[:, 1]
+    #print('xmax '+str(xmax)+'ymax '+str(ymax)+'xmin '+str(xmin)+'ymin '+str(ymin))
+    if xmax - xmin > ymax - ymin:
+        differencemax = (xmax - xmin / 2) + 10
     else:
-        differencemax = (ymax-ymin/2)+10
+        differencemax = (ymax - ymin / 2) + 10
     experiments = 100
     randomDictionary = {}
     for i in range(experiments):
-        distancelist = []    
-        #Se genera un punto aleatorio (x,y)
+        distancelist = []
+        # Se genera un punto aleatorio (x,y)
         xa = randint(xmin, xmax)
         ya = randint(ymin, ymax)
-        #Se calculan las distancias entre el punto aleatorio y los puntos de referencia
+        # Se calculan las distancias entre el punto aleatorio y los puntos de referencia
         for p in contour:
-            distance = math.sqrt(pow(p[:,0]-xa,2)+pow(p[:,1]-ya,2))
+            distance = math.sqrt(pow(p[:, 0] - xa, 2) + pow(p[:, 1] - ya, 2))
             distancelist.append(distance)
-        #Si la diferencia de distancias es menor a la establecida, se guarda el punto
-        if (max(distancelist)-min(distancelist))<=differencemax:
+        # Si la diferencia de distancias es menor a la establecida, se guarda el punto
+        if (max(distancelist) - min(distancelist)) <= differencemax:
             randomDictionary[xa] = [ya]
     pointslist = randomDictionary.items()
-    #Se calcula el promedio de los puntos validos
+    # Se calcula el promedio de los puntos validos
     summationx = 0
-    summationy = 0    
+    summationy = 0
     #print('longitud lista '+str(len(pointslist)))
     for x, y in pointslist:
-        #print(str(y[0]))
-        summationx += x 
+        # print(str(y[0]))
+        summationx += x
         summationy += y[0]
-    averagex = summationx/len(pointslist)
-    averagey = summationy/len(pointslist)
+    averagex = summationx / len(pointslist)
+    averagey = summationy / len(pointslist)
 
-    print('Centro geometrico -> x: '+str(averagex)+'y: '+str(averagey))
+    print('Centro geometrico -> x: ' + str(averagex) + 'y: ' + str(averagey))
     dibujo.point((averagex, averagey), fill="white")
-    dibujo.text((averagex,averagey), '('+str(averagex)+','+str(averagey)+')', 
-    font=None, fill=(255,255,255,255))
+    dibujo.text((averagex, averagey), '(' + str(averagex) + ',' + str(averagey) + ')',
+                font=None, fill=(255, 255, 255, 255))
 
 imagenres.show()
 imagenres.save("linea.png")
@@ -131,7 +131,7 @@ while(1):
     # Mostrar la mascara final y la imagen
     cv2.imshow('Finale', mask)
     cv2.imshow('Imagen', imagen)
-    cv2.imshow('ImagenR', imagenres)    
+    cv2.imshow('ImagenR', imagenres)
     tecla = cv2.waitKey(5) & 0xFF
     if tecla == 27:
         break
